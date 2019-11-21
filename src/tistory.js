c95180a3627f37e386f7e149799c9a1383cf46ba
haplesspanda <57515852+haplesspanda@users.noreply.github.com>
2019-11-20 18:49:32 -0800

    Add support for more tistory site formats

--- src/tistory.js
+++ src/tistory.js
@@ -2,15 +2,18 @@
 import {hasTextArea, createTextArea} from './common';
 
 (function tistoryExecFn() {
-  // TODO: make this work for old tistory too?  Need to test on more sites.
-  const dateMatch = document.querySelector('.title').innerText.match(/\b(20)?([0-9]{6})\b/);
+  // TODO: Need to test on more sites, this works for a few at least
+  const title = document.querySelector('#content h2') || document.querySelector('.title');
+  const dateMatch = title.innerText.match(/\b(20)?([0-9]{6})\b/);
   const formattedDate = dateMatch && dateMatch.length >= 3 && dateMatch[2] || null;
   const currentUrl = window.location.href;
-  const srcs = Array.from(document.querySelectorAll('.contents img')).map(img => img.getAttribute('src'));
   
-  const title = document.querySelector('.entryTitle');
-  if (!hasTextArea(title)) {
+  const contents = document.querySelector('.contents') || document.querySelector('.article');
+  const srcs = Array.from(contents.querySelectorAll('img')).map(img => img.getAttribute('src'));
+  
+  const textareaParent = document.querySelector('#head') || document.querySelector('.entryTitle') || title;
+  if (!hasTextArea(textareaParent)) {
     const textArea = createTextArea(formattedDate, currentUrl, srcs, '300px');
-    title.appendChild(textArea);    
+    textareaParent.appendChild(textArea);    
   }
 })();
