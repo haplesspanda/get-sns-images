ba92c31b31574f881f81619bfd9d0d7ef6a5e465
haplesspanda <57515852+haplesspanda@users.noreply.github.com>
2022-07-12 00:53:26 -0700

    Fix instagram
    Removes instagram video support, updates previous/next button clicking for instagram

--- src/instagram.ts
+++ src/instagram.ts
@@ -12,118 +12,92 @@ import {Schedule} from './types';
     permalinkUrl.search = '';
     const permalink = permalinkUrl.toString();
 
-    const extraInfoRequest = new XMLHttpRequest();
-    extraInfoRequest.addEventListener('load', function (this: XMLHttpRequest) {
-      const json = JSON.parse(this.responseText);
-      function getVideoUrl(index: number) {
-        let result = '';
-        try {
-          const graphqlEdges =
-            json['graphql']['shortcode_media']['edge_sidecar_to_children'][
-              'edges'
-            ];
-          if (index >= graphqlEdges.length) {
-            return '';
-          }
+    const dialogArticle = document.querySelector('div[role="dialog"] article');
+    const mainArticle = document.querySelector('main[role="main"] article');
 
-          result = graphqlEdges[index]['node']['video_url'];
-        } catch (e) {
-          console.error('failed to get non-blob video url');
-        }
-        return result;
-      }
+    const article = dialogArticle ?? mainArticle;
+    if (!article) {
+      return;
+    }
 
-      const dialogArticle = document.querySelector(
-        'div[role="dialog"] article'
+    const isDialogContext = dialogArticle != null;
+
+    const articleParent = article.parentElement;
+    const context = articleParent ?? document;
+
+    // Dialog context doesn't let us append below the dialog and still interact with buttons.
+    // Need to add to the right pane of the dialog instead.
+    const textAreaTarget =
+      (isDialogContext
+        ? article.querySelector('section')?.parentElement
+        : context) ?? document;
+
+    const timeElement = article.querySelector('a time');
+    const time = timeElement?.getAttribute('datetime');
+    const formattedDateString = time ? formatDate(time) : null;
+    const srcs: string[] = [];
+
+    // 1) Go back to first image.
+    // 2) Advance and add images to list of srcs.
+    // 3) Create and add textarea with all srcs.
+
+    function getNavigationButtons() {
+      return Array.from(
+        context
+          .querySelector('div[role="presentation"]')
+          ?.parentElement?.querySelectorAll(':scope > button') || []
       );
-      const mainArticle = document.querySelector('main[role="main"] article');
+    }
 
-      const article = dialogArticle ?? mainArticle;
-      if (!article) {
-        return;
+    function goToStart() {
+      const previousButton = getNavigationButtons().filter(
+        button =>
+          window.getComputedStyle(button).getPropertyValue('left') === '0px'
+      )[0];
+      if (previousButton) {
+        (previousButton as HTMLButtonElement).click();
+        setTimeout(goToStart, 10);
+      } else {
+        appendImagesAndAdvance();
       }
+    }
 
-      const isDialogContext = dialogArticle != null;
+    goToStart();
 
-      const articleParent = article.parentElement;
-      const context = articleParent ?? document;
+    function appendImagesAndAdvance() {
+      const images: HTMLImageElement[] = Array.from(
+        context.querySelectorAll('img[srcset]')
+      );
+      const newSrcs = images.map(image => image.src);
+      newSrcs.forEach(newSrc => {
+        if (newSrc && !srcs.includes(newSrc)) {
+          srcs.push(newSrc);
+        }
+      });
 
-      // Dialog context doesn't let us append below the dialog and still interact with buttons.
-      // Need to add to the right pane of the dialog instead.
-      const textAreaTarget =
-        (isDialogContext
-          ? article.querySelector('section')?.parentElement
-          : context) ?? document;
-
-      const timeElement = article.querySelector('a time');
-      const time = timeElement?.getAttribute('datetime');
-      const formattedDateString = time ? formatDate(time) : null;
-      const srcs: string[] = [];
-
-      // 1) Go back to first image.
-      // 2) Advance and add images to list of srcs.
-      // 3) Create and add textarea with all srcs.
-
-      function goToStart() {
-        const previousButtonIcon = context.querySelector(
-          'button .coreSpriteLeftChevron'
-        );
-        const previousButton = previousButtonIcon?.parentElement;
-        if (previousButton) {
-          previousButton.click();
-          setTimeout(goToStart, 10);
-        } else {
-          appendImagesAndAdvance();
+      // If there are more images, go to the next one and run again.
+      const nextButton = getNavigationButtons().filter(
+        button =>
+          window.getComputedStyle(button).getPropertyValue('right') === '0px'
+      )[0];
+      if (nextButton) {
+        (nextButton as HTMLButtonElement).click();
+        setTimeout(appendImagesAndAdvance, 10);
+      } else {
+        // No more images - add textarea.
+        if (!hasTextArea(textAreaTarget)) {
+          const textArea = createTextArea(
+            {date: formattedDateString, fromContent: false},
+            permalink,
+            srcs,
+            schedule,
+            '300px',
+            '1000' // Absurdly-high flex order to appear at the bottom of right pane.
+          );
+          textAreaTarget.appendChild(textArea);
         }
       }
-
-      goToStart();
-
-      function appendImagesAndAdvance() {
-        const images: HTMLImageElement[] = Array.from(
-          context.querySelectorAll('img[srcset], video')
-        );
-        const newSrcs = images.map(image => image.src);
-        newSrcs.forEach(newSrc => {
-          // Handle blob srcs.
-          let srcToAdd = newSrc;
-          if (newSrc.startsWith('blob:')) {
-            srcToAdd = getVideoUrl(srcs.length);
-          }
-
-          if (srcToAdd && !srcs.includes(srcToAdd)) {
-            srcs.push(srcToAdd);
-          }
-        });
-
-        // If there are more images, go to the next one and run again.
-        const nextButtonIcon = context.querySelector(
-          'button .coreSpriteRightChevron'
-        );
-        const nextButton = nextButtonIcon?.parentElement;
-        if (nextButton) {
-          nextButton.click();
-          setTimeout(appendImagesAndAdvance, 10);
-        } else {
-          // No more images - add textarea.
-          if (!hasTextArea(textAreaTarget)) {
-            const textArea = createTextArea(
-              {date: formattedDateString, fromContent: false},
-              permalink,
-              srcs,
-              schedule,
-              '300px',
-              '1000' // Absurdly-high flex order to appear at the bottom of right pane.
-            );
-            textAreaTarget.appendChild(textArea);
-          }
-        }
-      }
-    });
-    const graphqlUrl = new URL(permalink);
-    graphqlUrl.searchParams.set('__a', '1');
-    extraInfoRequest.open('GET', graphqlUrl.toString(), true);
-    extraInfoRequest.send();
+    }
   }
 
   injectSchedule(execWithSchedule);
