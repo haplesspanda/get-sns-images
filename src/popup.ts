8774d61397401f87670c88ce339b4285d96e58bb
haplesspanda <57515852+haplesspanda@users.noreply.github.com>
2022-07-12 19:54:46 -0700

    Updates to manifest v3
    Manifest v3 is not supported in firefox yet

--- src/popup.ts
+++ src/popup.ts
@@ -46,10 +46,15 @@ const BUTTONS: ButtonSpec[] = [
   },
 ];
 
-function decorateButton(button: HTMLButtonElement, file: string, text: string) {
+function decorateButton(
+  button: HTMLButtonElement,
+  file: string,
+  text: string,
+  tab: chrome.tabs.Tab
+) {
   button.innerText = text;
   button.onclick = () => {
-    chrome.tabs.executeScript({file});
+    chrome.scripting.executeScript({target: {tabId: tab.id!}, files: [file]});
   };
 }
 
@@ -68,28 +73,29 @@ function decorateFakeButton(fakeButton: HTMLElement, handler: () => void) {
   fakeButton.onkeypress = handlerWrapper;
 }
 
-function showAutodetectedOption(button: ButtonSpec) {
+function showAutodetectedOption(button: ButtonSpec, tab: chrome.tabs.Tab) {
   decorateButton(
     document.getElementById('autodetected-button') as HTMLButtonElement,
     button.file,
-    button.text
+    button.text,
+    tab
   );
 
-  const seeAllOptions: HTMLElement = document.getElementById(
-    'see-all-options'
-  )!;
-  decorateFakeButton(seeAllOptions, showAllOptions);
+  const seeAllOptions: HTMLElement =
+    document.getElementById('see-all-options')!;
+  decorateFakeButton(seeAllOptions, () => showAllOptions(tab));
 
   document.getElementById('autodetect')!.style.display = 'block';
   document.getElementById('all-options')!.style.display = 'none';
 }
 
-function showAllOptions() {
+function showAllOptions(tab: chrome.tabs.Tab) {
   BUTTONS.forEach(button => {
     decorateButton(
       document.getElementById(button.buttonId) as HTMLButtonElement,
       button.file,
-      button.text
+      button.text,
+      tab
     );
   });
 
@@ -97,17 +103,26 @@ function showAllOptions() {
   document.getElementById('autodetect')!.style.display = 'none';
 }
 
-chrome.tabs.executeScript({file: 'dist/autodetect.js'});
+async function runAutoDetection() {
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  await chrome.scripting.executeScript({
+    target: {tabId: tab.id!},
+    files: ['dist/autodetect.js'],
+  });
+}
+runAutoDetection();
 
-chrome.runtime.onMessage.addListener((request: Message) => {
-  if (request.type === 'autodetect') {
-    const detectedButton = BUTTONS.find(
-      button => button.name === request.result
-    );
-    if (detectedButton) {
-      showAutodetectedOption(detectedButton);
-    } else {
-      showAllOptions();
+chrome.runtime.onMessage.addListener(
+  (request: Message, sender: chrome.runtime.MessageSender) => {
+    if (request.type === 'autodetect') {
+      const detectedButton = BUTTONS.find(
+        button => button.name === request.result
+      );
+      if (detectedButton) {
+        showAutodetectedOption(detectedButton, sender.tab!);
+      } else {
+        showAllOptions(sender.tab!);
+      }
     }
   }
-});
+);
